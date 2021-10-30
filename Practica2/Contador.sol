14c2ae8807233fa4be69c8778859f6609e6a9bc5
JavierSanzSaez <51752232+JavierSanzSaez@users.noreply.github.com>
2021-10-30 21:50:47 +0200

    Empezando parte 2

--- Practica2/Contador.sol
+++ Practica2/Contador.sol
@@ -1,25 +1,14 @@
 /*
 Tarea 1
 
-El Smart Contract a usar en este ejercicio debe ser una modificación del contrato Contador.sol visto en clase.
+El Smart Contract a usar en este ejercicio debe ser una modificación del contrato 
+Contador.sol visto en clase.
 
 Debe modificarse añadiendo dos métodos nuevos:
 
     decr, que disminuye el valor actual del contador en una unidad.
     reset, que pone a 0 el valor actual del contador.
-
-Tarea 2
-
-Modifique la aplicación web desarrollada para añadir dos botones nuevos para disminuir en uno y poner a cero el valor del contrato Contador desplegado.
-
-El botón HTML usado para decremento debe tener "Decrementar" como título, y "cdecr" como valor del atributo id.
-
-El botón HTML usado para reiniciar debe tener "Reset" como título, y "creset" como valor del atributo id.
-
-Cree las funciones App.handleDecr() y App.handleReset() para manejar las pulsaciones sobre los nuevos botones. Estas funciones deber crear transacciones para invocar los métodos decr y reset del contrato Contador desplegado.
-
-Use el servidor serve para servir la app desarrollada.
- */
+*/
 
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
